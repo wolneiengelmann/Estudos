99d9ec75c1f5de25faf0ce639b9b5e03ce5b8add
wolneiengelmann <wolnei.guitar@gmail.com>
2022-02-14 21:24:16 -0300

    conversões para tipos de dados

--- Aula 2 conversões JavaScript - Alura.js
+++ Aula 2 conversões JavaScript - Alura.js
@@ -11,5 +11,11 @@ console.log(numero == numeroString) // comparar apenas o valor
 console.log(numero === numeroString) // comparar o valor e o tipo
 // caso eu utilize o sinal de + o resultado será a concatenação dos valores
 console.log(numero + numeroString)
-//para uma conversão implícita utiliza-se o 'Number' 
+//para uma conversão implícita utiliza-se o 'Number' ou String que vão nos 
+//permitir converter os dados para números ou textos.
 console.log(numero + Number(numeroString))
+//Podemos usar o operador de soma + para fazer a conversão de textos para números,
+//colocando-os antes das variáveis:
+
+//console.log( + largura * + altura); 
+//teremos a conversão de String para números realizado usando o + antes das variáveis
