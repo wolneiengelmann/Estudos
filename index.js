70de4761947112dcda0f2e217e51afe351e5ce98
wolneiengelmann <wolnei.guitar@gmail.com>
2022-02-17 16:06:20 -0300

    Aula 1 de array utilizando "length"

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 //medias 10, 6, 5 8, 7,5
 
-let nota1 = 10;
-let nota2 = 6.5;
-let nota3 = 8;
-let nota4= 7.5;
+// let nota1 = 10;
+// let nota2 = 6.5;
+// let nota3 = 8;
+// let nota4= 7.5;
+            // 0    1   2   3
+const notas = [10, 6.5, 8, 7.5]
 
-let media = (nota1 + nota2 + nota3 + nota4) / 4
+//let media = (nota1 + nota2 + nota3 + nota4) / 4
 
-console.log(media)
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) 
+/ notas.length
+
+console.log("A média é " + media +".")
