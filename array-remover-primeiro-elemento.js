67bca58c2a1c3924b38ba1432f74796cef31bb50
wolneiengelmann <wolnei.guitar@gmail.com>
2022-03-01 13:52:07 -0300

    comando shift é utilizado para remover 1 elemento no parametro elemento do array

---
+++ array-remover-primeiro-elemento.js
@@ -0,0 +1,7 @@
+//o comando shift é utilizado para remover 1 elemento no
+// parametro elemento do array
+let nomes = ["Matheus", "Joao", "Pedro"]
+let nome = nomes.shift()
+
+console.log(nome)
+console.log(nomes)
