db979140bb68d98cadaf41e5de52c87a6a1aa38e
wolneiengelmann <wolnei.guitar@gmail.com>
2022-03-01 14:52:42 -0300

    Método para juntar/concatenar (concat).

---
+++ Juntando-turmas.js
@@ -0,0 +1,5 @@
+const salaDePython = [`Melissa`, `Helena`, `rodrigo`]
+const salaDeJavaScript = [`Ju`,`Leo`, `Raquel`]
+
+const salasUnificadas = salaDePython.concat(salaDeJavaScript)
+console.log (salasUnificadas)
