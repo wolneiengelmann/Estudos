c645b4829326995fa467876d25d6f3e172e07440
wolneiengelmann <wolnei.guitar@gmail.com>
2022-03-28 17:10:13 -0300

    linha de comando com a condição "FOR".

---
+++ For.js
@@ -0,0 +1,6 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+                    //6
+for (let i = 0; i < numeros.length; i++){ // ultima ação executada é o i++ (i= i+1)
+            //i=0 numeros no indice [0]
+    console.log (i, numeros[i])
+} 
