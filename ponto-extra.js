e1cbb69a1d942d24d91c2d23acc65287b0bb9a1c
wolneiengelmann <wolnei.guitar@gmail.com>
2022-03-31 15:52:12 -0300

    utilizando "map" para retornar array atualizada

--- ponto-extra.js
+++ ponto-extra.js
@@ -1,5 +1,5 @@
 const notas = [10,9,8,7,6]
 
 const notasAtualizadas = notas.map( nota => nota == 10 ? nota : ++nota)
-//utilizando o map retorna dados atualizado de array
+//utilizando o map retorna dados atualizados de array
 console.log(notasAtualizadas)
