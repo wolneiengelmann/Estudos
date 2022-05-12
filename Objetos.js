285c66f78133819c11e61cc3b11527f48e803cb9
wolneiengelmann <wolnei.guitar@gmail.com>
2022-05-12 14:56:10 -0300

    função para criar objeto no js

---
+++ Objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = ["12365242625","9242624265","93214867863"]
+
+// const cliente ["nome","André","idade",36]
+
+const cliente = {
+    nome: "Andre",
+    idade:36,
+    cpf:"12653652266",
+    email:"andre@email.com"
+}
