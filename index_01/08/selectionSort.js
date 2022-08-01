c21a9d3e72db1bc2fb5bb2c4ed96efdcebc7efe2
wolneiengelmann <wolnei.guitar@gmail.com>
2022-08-01 15:49:00 -0300

    uso de métodos melhorados para organizar um array

--- index_01/08/selectionSort.js
+++ index_01/08/selectionSort.js
@@ -5,11 +5,12 @@ for(let atual = 0; atual < livros.length; atual++){
     let menor = menorValor(livros, atual);
 
     let livroAtual = livros[atual];
+    console.log('Posição atual', atual);
     console.log('Livro atual', livros[atual]);
+    //console.log('Posição atual', atual);
     let livroMenorPreco = livros[menor];
     console.log('Livro menor preço - ', livros[menor]);
     livros[atual] = livroMenorPreco;
     livros[menor] = livroAtual;
 }
-
-console.log(livros);
+console.log(livros)
